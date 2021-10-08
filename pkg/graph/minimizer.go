5edd64294260f9cf3367d8323345c5a8da3fdee8
AIwaifus <aiwaifusoy@proton.me>
2021-10-08 11:12:23 +0000

    link to github

---
+++ pkg/graph/minimizer.go
@@ -0,0 +1,27 @@
+
+package graph
+
+type Minimizer struct {
+	Layer
+	cursor       int
+	batch        [][][]float64
+	gradients    [][]float64
+	learningRate LearningRate
+	optimizer    Optimizer
+	regularizer  Regularizer
+	weights      [][]float64
+}
+
+func (m *Minimizer) Minimize(gradients []float64) []float64 {
+	gradients = m.Layer.Minimize(gradients)
+	for i := range m.gradients {
+		copy(m.batch[m.cursor][i], m.gradients[i])
+	}
+	m.cursor++
+	if m.cursor < len(m.batch) {
+		return gradients
+	}
+	m.cursor = 0
+	m.average()
+	m.regularize()
+	m.optimize()
