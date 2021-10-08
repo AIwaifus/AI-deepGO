a9d9a34204cbf8c541e2a8c58dfc5bb35f49fbc2
AIwaifus <aiwaifusoy@proton.me>
2021-10-08 19:02:09 +0000

    Updated travis configuration

--- pkg/graph/minimizer.go
+++ pkg/graph/minimizer.go
@@ -25,3 +25,35 @@ func (m *Minimizer) Minimize(gradients []float64) []float64 {
 	m.average()
 	m.regularize()
 	m.optimize()
+	lr := m.learningRate.Rate()
+	for i := range m.batch[0] {
+		for j := range m.batch[0][i] {
+			m.weights[i][j] -= lr * m.batch[0][i][j]
+		}
+	}
+	return gradients
+}
+
+func (m *Minimizer) average() {
+	batchSize := len(m.batch)
+	n := float64(batchSize)
+	for i := range m.batch[0] {
+		for j := range m.batch[0][i] {
+			m.batch[0][i][j] = m.batch[0][i][j] / n
+		}
+	}
+	for i := 1; i < int(batchSize); i++ {
+		for j := range m.batch[0] {
+			for k := range m.batch[0][j] {
+				m.batch[0][j][k] += m.batch[i][j][k] / n
+			}
+		}
+	}
+}
+
+func (m *Minimizer) optimize() {
+	if m.optimizer == nil {
+		return
+	}
+	g := m.optimizer.Optimize(m.batch[0])
+	for i := range g {
