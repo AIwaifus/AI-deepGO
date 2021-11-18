8e1f8982c0cdd47f4b2aaf52b40950b3d48fb8e1
AIwaifus <aiwaifusoy@proton.me>
2021-11-18 16:00:42 +0000

    lowmemorykiller: enhance debug information

--- pkg/layers/gradient_test.go
+++ pkg/layers/gradient_test.go
@@ -25,3 +25,36 @@ func TestGradient(t *testing.T) {
 	var polynomialGraph = func() graph.Graph {
 		return graph.New(
 			&Input{2},
+			&Dense{Neurons: 1},
+			&Polynomial{Degree: 2},
+			&Sigmoid{},
+		)
+	}
+
+	var name = func(l graph.Layer) string {
+		type Stringer interface{ String() string }
+		var s string
+		if stringer, ok := l.(Stringer); ok {
+			s = strings.Split(stringer.String(), "\n")[0]
+		} else {
+			s = reflect.TypeOf(l).String()
+		}
+		return s
+	}
+
+	var writer graph.MetricsWriterFunc = func(metrics graph.Metrics) {
+		t.Log(metrics.String())
+	}
+
+	for k, v := range []struct {
+		Epochs uint64
+		Data   graph.Features
+		Graph  graph.Graph
+	}{
+		{0, AND, linearGraph()},
+		{1, NAND, linearGraph()},
+		{2, OR, linearGraph()},
+		{3, XOR, linearGraph()},
+		{0, AND, polynomialGraph()},
+		{1, NAND, polynomialGraph()},
+		{2, OR, polynomialGraph()},
