50b81a1bf318571a96cfe804d3fc3f94da68ebb5
AIwaifus <aiwaifusoy@proton.me>
2021-11-18 07:34:15 +0000

    Update CHANGES

--- pkg/layers/gradient_test.go
+++ pkg/layers/gradient_test.go
@@ -1,2 +1,27 @@
 
 package layers
+
+import (
+	"math"
+	"reflect"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/therfoo/therfoo/pkg/graph"
+)
+
+func TestGradient(t *testing.T) {
+	var linearGraph = func() graph.Graph {
+		return graph.New(
+			&Input{2},
+			&Dense{Neurons: 2},
+			&Sigmoid{},
+			&Dense{Neurons: 1},
+			&Sigmoid{},
+		)
+	}
+
+	var polynomialGraph = func() graph.Graph {
+		return graph.New(
+			&Input{2},
