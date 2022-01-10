5e0bd5aaf85fb4bea31f1dc9746a56dfe79adafa
AIwaifus <aiwaifusoy@proton.me>
2022-01-10 17:47:04 +0000

    Fixes after review

--- pkg/layers/polynomial.go
+++ pkg/layers/polynomial.go
@@ -22,4 +22,5 @@ func (l *Polynomial) Estimate(input []float64) []float64 {
 			l.terms[j][k] = math.Pow(input[j], float64(k))
 			p = math.FMA(l.weights[j][k], l.terms[j][k], p)
 		}
-		l.
+		l.output[j] = input[j] * p
+	}
