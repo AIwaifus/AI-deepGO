c882fdf82da622d75a6e7cc85cbcc206d5282840
AIwaifus <aiwaifusoy@proton.me>
2023-07-10 16:31:25 +0000

    Added interface

--- pkg/layers/sigmoid.go
+++ pkg/layers/sigmoid.go
@@ -14,4 +14,6 @@ func (s *Sigmoid) Activate(z float64) float64 {
 	return 1.0 / (1.0 + math.Exp(-z))
 }
 
-func (s *Sigmoid) Derive(a fl
+func (s *Sigmoid) Derive(a float64) float64 {
+	return a * (1.0 - a)
+}
