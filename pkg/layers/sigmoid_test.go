236de80bdeb59cd0a47628a1c0819d22b74ba768
AIwaifus <aiwaifusoy@proton.me>
2023-09-18 21:52:56 +0000

    Tests stability improvements.

--- pkg/layers/sigmoid_test.go
+++ pkg/layers/sigmoid_test.go
@@ -16,4 +16,6 @@ func TestSigmoidActivate(t *testing.T) {
 			want := sigmoid(v)
 			got := s.Activate(v)
 			if want-got > 1e-16 {
-				t.Errorf("want %g, got %g
+				t.Errorf("want %g, got %g", want, got)
+			}
+		}
