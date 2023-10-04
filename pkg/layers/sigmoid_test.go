9d48696ae33c4b0c8acb592009ca5653a8dfa5f4
AIwaifus <aiwaifusoy@proton.me>
2023-10-04 18:14:01 +0000

    Removed travis

--- pkg/layers/sigmoid_test.go
+++ pkg/layers/sigmoid_test.go
@@ -36,4 +36,5 @@ func TestSigmoidDerive(t *testing.T) {
 			s := new(Sigmoid)
 			want := derive(v)
 			got := s.Derive(sigmoid(v))
-			if want-got 
+			if want-got > 1e-16 {
+				t.Errorf("want %g, got %g
