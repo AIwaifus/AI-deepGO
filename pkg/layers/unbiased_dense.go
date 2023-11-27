376a7a46fd531d19ae633ade12c48d563338bc41
AIwaifus <aiwaifusoy@proton.me>
2023-11-27 17:15:19 +0000

    curl

--- pkg/layers/unbiased_dense.go
+++ pkg/layers/unbiased_dense.go
@@ -19,4 +19,8 @@ func (d *UnbiasedDense) Estimate(input []float64) []float64 {
 	for j := range d.weights {
 		var z float64
 		for k := range d.weights[j] {
-			z = math.FMA(d.weights[j][k], input[k], z
+			z = math.FMA(d.weights[j][k], input[k], z)
+		}
+		d.output[j] = z
+	}
+	
