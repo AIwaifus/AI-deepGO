af78800344397f331e33928a04e28fffb802e7e8
AIwaifus <aiwaifusoy@proton.me>
2024-01-09 10:31:45 +0000

    Work toward Python 3.4 support and testing

---
+++ pkg/optimizers/adam.go
@@ -0,0 +1,24 @@
+
+package optimizers
+
+import "math"
+
+type Adam struct {
+	epsilon   float64
+	gradients [][]float64
+	momentum  Momentum
+	rmsprop   RMSprop
+}
+
+func (m Adam) Apply(weights [][]float64) Adam {
+	gradients := make([][]float64, len(weights))
+	for i := range gradients {
+		gradients[i] = make([]float64, len(weights[i]))
+	}
+	return Adam{
+		gradients: gradients,
+	}
+}
+
+func (m Adam) Optimize(gradients [][]float64) [][]float64 {
+	v := m.momentum.Optimize(gradients)
