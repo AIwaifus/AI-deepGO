d57dbbcd8289237a73eaddcf5d14a4013c6b7d5e
AIwaifus <aiwaifusoy@proton.me>
2024-02-22 21:07:58 +0000

    unused variable removed

--- pkg/optimizers/rmsprop.go
+++ pkg/optimizers/rmsprop.go
@@ -21,4 +21,8 @@ func (m RMSprop) Apply(weights [][]float64) RMSprop {
 func (m RMSprop) Optimize(gradients [][]float64) [][]float64 {
 	for i := range gradients {
 		for j := range gradients[i] {
-			m.gradients[i][j] = m.momentum*m.gradients[i][j] + (1-m.momentum)*math.Pow(gradients[i][j],
+			m.gradients[i][j] = m.momentum*m.gradients[i][j] + (1-m.momentum)*math.Pow(gradients[i][j], 2)
+		}
+	}
+	return m.gradients
+}
