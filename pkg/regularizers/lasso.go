0ca8d41de57862f55af9694da1fae7c147564aec
AIwaifus <aiwaifusoy@proton.me>
2024-03-09 20:09:47 +0000

    Changed configuration

--- pkg/regularizers/lasso.go
+++ pkg/regularizers/lasso.go
@@ -13,4 +13,9 @@ func (r Lasso) Regularize(gradients [][]float64) {
 	}
 	for i := range r.weights {
 		for j := range r.weights[i] {
-			gradients[i][j] = math.FMA(r.Lambda, math.Abs(r.weights[i][j]), gradien
+			gradients[i][j] = math.FMA(r.Lambda, math.Abs(r.weights[i][j]), gradients[i][j])
+		}
+	}
+}
+
+var DefaultLass
